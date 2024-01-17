dba7bb6584260dae43468d871258cbd50cb3e71a
Nick Semenkovich <semenko@alum.mit.edu>
2024-01-17 15:49:32 -0600

    adjust verbose

--- src/bam2tensor/__main__.py
+++ src/bam2tensor/__main__.py
@@ -58,9 +58,8 @@ def main(
     """Bam2Tensor."""
     time_start = time.time()
     # Print run information
-    if verbose:
-        print(f"Reference fasta: {reference_fasta}")
-        print(f"Input path: {input_path}")
+    print(f"Reference fasta: {reference_fasta}")
+    print(f"Input path: {input_path}")
 
     # Check if input_path is a file or a directory
     if os.path.isfile(input_path):
@@ -75,10 +74,9 @@ def main(
     else:
         raise ValueError(f"Input path {input_path} is not a file or a directory.")
 
-    if verbose:
-        print(f"\nFound {len(bams_to_process)} .bam file(s) to process:")
-        for bam_file in bams_to_process:
-            print(f"\t{bam_file}")
+    print(f"\nFound {len(bams_to_process)} .bam file(s) to process:")
+    for bam_file in bams_to_process:
+        print(f"\t{bam_file}")
 
     # Check input/output validity
     for bam_file in bams_to_process:
@@ -88,10 +86,9 @@ def main(
         # Check if the output files exist or are writable
         if os.path.exists(output_file):
             if overwrite:
-                if verbose:
-                    print(
-                        "\t\tOutput file exists and --overwrite specified. Will overwrite existing .methylation.npz file."
-                    )
+                print(
+                    "\t\tOutput file exists and --overwrite specified. Will overwrite existing .methylation.npz file."
+                )
                 assert os.access(
                     output_file, os.W_OK
                 ), f"Output file is not writable: {output_file}"
@@ -106,10 +103,7 @@ def main(
                 os.path.dirname(os.path.abspath(output_file)), os.W_OK
             ), f"Output file path is not writable: {output_file}"
 
-    if verbose:
-        print(
-            f"\nLoading (or generating) methylation embedding named: {reference_fasta}"
-        )
+    print(f"\nLoading (or generating) methylation embedding named: {reference_fasta}")
 
     ## Globals
     HG38_CHROMOSOMES = ["chr" + str(i) for i in range(1, 23)] + ["chrX", "chrY"]
@@ -129,8 +123,7 @@ def main(
         verbose=verbose,
     )
 
-    if verbose:
-        print(f"\nTime elapsed: {time.time() - time_start:.2f} seconds")
+    print(f"\nTime elapsed: {time.time() - time_start:.2f} seconds")
 
     #################################################
     # Operate over the input BAM files
@@ -140,10 +133,9 @@ def main(
         time_bam = time.time()
         output_file = os.path.splitext(input_bam)[0] + ".methylation.npz"
         # Extract methylation data as a COO sparse matrix
-        if verbose:
-            print("\n" + "=" * 80)
-            print(f"Processing BAM file {i+1} of {len(bams_to_process)}")
-            print(f"\nExtracting methylation data from: {input_bam}")
+        print("\n" + "=" * 80)
+        print(f"Processing BAM file {i+1} of {len(bams_to_process)}")
+        print(f"\nExtracting methylation data from: {input_bam}")
 
         methylation_data_coo = extract_methylation_data_from_bam(
             input_bam=input_bam,
@@ -153,19 +145,16 @@ def main(
             debug=debug,
         )
 
-        if verbose:
-            print(f"\nWriting methylation data to: {output_file}")
+        print(f"\nWriting methylation data to: {output_file}")
 
         # Save the matrix, which is an ndarray of shape (n_reads, n_cpgs), to a file
         scipy.sparse.save_npz(output_file, methylation_data_coo, compressed=True)
 
         # Report performance time
-        if verbose:
-            print(f"\nTime for this bam: {time.time() - time_bam:.2f} seconds")
-            print(f"\nTotal time elapsed: {time.time() - time_start:.2f} seconds")
+        print(f"\nTime for this bam: {time.time() - time_bam:.2f} seconds")
+        print(f"\nTotal time elapsed: {time.time() - time_start:.2f} seconds")
 
-    if verbose:
-        print("\nRun complete.")
+    print("\nRun complete.")
 
 
 if __name__ == "__main__":
