47039cff56c11fe1bb40a045426cc813b703c93e
Nick Semenkovich <semenko@alum.mit.edu>
2024-01-18 10:09:53 -0600

    Restructure main()

--- src/bam2tensor/__main__.py
+++ src/bam2tensor/__main__.py
@@ -1,7 +1,6 @@
 # Import modules
 import click
 import os
-import sys
 import time
 
 # Third party modules
@@ -12,6 +11,57 @@ from bam2tensor.embedding import GenomeMethylationEmbedding
 from bam2tensor.functions import (
     extract_methylation_data_from_bam,
 )
+
+
+def get_input_bams(input_path: str) -> list:
+    """Determine if the input is a path or file, and return a list of .bam files to process.
+
+    Args:
+        input_path (str): Input path or file.
+
+    Returns:
+        list: List of .bam files to process.
+    """
+    # Check if input_path is a file or a directory
+    if os.path.isfile(input_path):
+        return [input_path]
+    elif os.path.isdir(input_path):
+        # Recursively find all .bam files in this path, and add them to a list
+        bams_to_process = []
+        for root, _, files in os.walk(input_path):
+            for file in files:
+                if file.endswith(".bam"):
+                    bams_to_process.append(os.path.join(root, file))
+        return bams_to_process
+    else:
+        raise ValueError(f"Input path {input_path} is not a file or a directory.")
+
+
+def validate_input_output(bams_to_process: list, overwrite: bool) -> None:
+    """Validate the input and output files."""
+
+    # Check input/output validity
+    for bam_file in bams_to_process:
+        if not os.access(bam_file, os.R_OK):
+            raise ValueError(f"Input file is not readable: {bam_file}")
+
+        output_file = os.path.splitext(bam_file)[0] + ".methylation.npz"
+        # Check if the output files exist or are writable
+        if os.path.exists(output_file):
+            if overwrite:
+                print(
+                    "\t\tOutput file exists and --overwrite specified. Will overwrite existing .methylation.npz file."
+                )
+                if not os.access(output_file, os.W_OK):
+                    raise ValueError(f"Output file is not writable: {output_file}")
+            else:
+                raise ValueError(
+                    f"Output file exists and --overwrite not specified: {output_file}"
+                )
+        # Otherwise, check the path is writable
+        else:
+            if not os.access(os.path.dirname(os.path.abspath(output_file)), os.W_OK):
+                raise ValueError(f"Output file path is not writable: {output_file}")
 
 
 @click.command(
@@ -83,47 +133,20 @@ def main(
 
     print(f"\nTime elapsed: {time.time() - time_start:.2f} seconds")
 
-    # Check if input_path is a file or a directory
-    if os.path.isfile(input_path):
-        bams_to_process = [input_path]
-    elif os.path.isdir(input_path):
-        # Recursively find all .bam files in this path, and add them to a list
-        bams_to_process = []
-        for root, _, files in os.walk(input_path):
-            for file in files:
-                if file.endswith(".bam"):
-                    bams_to_process.append(os.path.join(root, file))
-    else:
-        raise ValueError(f"Input path {input_path} is not a file or a directory.")
+    #################################################
+    # Establish input/output for .bam files
+    #################################################
+
+    bams_to_process = get_input_bams(input_path)
 
     print(f"\nFound {len(bams_to_process)} .bam file(s) to process:")
     for bam_file in bams_to_process:
         print(f"\t{bam_file}")
 
-    # Check input/output validity
-    for bam_file in bams_to_process:
-        assert os.access(bam_file, os.R_OK), f"Input file is not readable: {bam_file}"
-
-        output_file = os.path.splitext(bam_file)[0] + ".methylation.npz"
-        # Check if the output files exist or are writable
-        if os.path.exists(output_file):
-            if overwrite:
-                print(
-                    "\t\tOutput file exists and --overwrite specified. Will overwrite existing .methylation.npz file."
-                )
-                assert os.access(
-                    output_file, os.W_OK
-                ), f"Output file is not writable: {output_file}"
-            else:
-                print(
-                    f"Exiting. An output file exists and --overwrite not specified: {output_file}"
-                )
-                sys.exit(1)
-        # Otherwise, check the path is writable
-        else:
-            assert os.access(
-                os.path.dirname(os.path.abspath(output_file)), os.W_OK
-            ), f"Output file path is not writable: {output_file}"
+    validate_input_output(
+        bams_to_process=bams_to_process,
+        overwrite=overwrite,
+    )
 
     #################################################
     # Operate over the input BAM files
