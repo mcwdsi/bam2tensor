124bc2fc8d857c0de7c2378cb0aeca24529068a2
Nick Semenkovich <semenko@alum.mit.edu>
2024-01-18 12:55:50 -0600

    + tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,4 +1,27 @@
+from bam2tensor import embedding
+from bam2tensor import functions
+
+
+TEST_EMBEDDING = embedding.GenomeMethylationEmbedding(
+    "test_genome",
+    expected_chromosomes=["chr1", "chr2", "chr3"],
+    fasta_source="tests/test_fasta.fa",
+    window_size=150,
+    skip_cache=False,
+    verbose=False,
+)
+
+
 def test_extract_methylation_data_from_bam() -> None:
     """Test extract_methylation_data_from_bam."""
 
-    return
+    test_coo_matrix = functions.extract_methylation_data_from_bam(
+        input_bam="tests/test.bam",
+        genome_methylation_embedding=TEST_EMBEDDING,
+        quality_limit=20,
+        verbose=False,
+        debug=False,
+    )
+
+    # Need to make a better test bam & fa pair...
+    assert test_coo_matrix.shape == (0, 37489)
